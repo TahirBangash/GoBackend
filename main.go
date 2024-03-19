b2dc2fc4666b630f4f7dde024125b823c66a124c
TahirBangash <tahir.bangash8805@gmail.com>
2024-03-19 11:17:08 -0400

    FEAT: GET, EDIT, CREATE, DELETE posts

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"GoBackendProject/controllers"
 	"GoBackendProject/initializers"
 
 	"github.com/gin-gonic/gin"
@@ -13,10 +14,12 @@ func init() {
 
 func main(){
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostsIndex)
+	r.GET("/posts/:id", controllers.PostsShow)
+	r.PUT("/posts/:id", controllers.PostsUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
+
 	r.Run()
 }
